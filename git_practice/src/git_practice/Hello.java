e65142197571bc7418aae3f81282c45104aab0f8
kosa <kosa@DESKTOP-S7IDGJ0>
2025-04-29 14:10:02 +0900

    git 연습중

--- git_practice/src/git_practice/Hello.java
+++
@@ -1,7 +0,0 @@
-package git_practice;
-
-public class Hello {
-	public static void main(String[] args) {
-		System.out.println("협업 준비 완료");
-	}
-}
---
+++ team2/src/team2/Git_practice.java
@@ -0,0 +1,7 @@
+package team2;
+
+public class Git_practice {
+	public static void main(String[] args) {
+		System.out.println("Git 연습중");
+	}
+}
